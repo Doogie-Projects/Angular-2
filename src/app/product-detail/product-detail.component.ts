8abc4493122fa23ee04fedb187b65836aa90d20d
ALFAROSO <alberth.sotelo@gmail.com>
2025-03-29 15:57:02 -0700

    display dynamic products-detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product, productsList } from '../products/products.mock';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,16 +9,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
 
-  producto: string = '';
-  color: string = '';
+  product?: Product;
+  color?: Product;
+  producList: Product[] = productsList
 
   constructor(private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this._route.params.subscribe(params => {
       // console.log(params['productId']);
-      this.producto = params['productId'];
-      this.color = params['category'];
+      // this.product = params['productId'];
+      this.product = this.producList.find(product => product.id == params['productId']);
+      // this.color = params['category'];
+      this.color = this.producList.find(product => product.color == params['category']);
+
     });
   }
 
