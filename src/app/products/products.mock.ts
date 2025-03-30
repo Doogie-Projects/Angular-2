369967f0a4b410f8625e867021998ea1b866714f
ALFAROSO <alberth.sotelo@gmail.com>
2025-03-30 07:44:54 -0700

    Add api service

--- src/app/products/products.mock.ts
+++ src/app/products/products.mock.ts
@@ -1,7 +1,7 @@
 export const productsList:Product[] = [
     {id:1, name:'Product 1', price:10, description:'Description 1', color:'yellow'},
     {id:2, name:'Product 2', price:20, description:'Description 2', color:'blue'},
-    {id:3, name:'Product 3', price:30, description:'Description 3', color:'green'},
+    {id:3, name:'Product 3', price:30, description:'Description 3', color:'pink'},
     {id:4, name:'Product 4', price:40, color:'red'},    
     {id:5, name:'Product 5', price:50, description:'Description 5', color:'orange'},
 
