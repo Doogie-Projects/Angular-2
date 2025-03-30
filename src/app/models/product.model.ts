920c3f65cd09b50c2f30ac04baea9b1d6588af7f
ALFAROSO <alberth.sotelo@gmail.com>
2025-03-30 08:23:49 -0700

    Add code to perform CRUD

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,21 @@
+export interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: Category;
+  image: string;
+  rating: Rating;
+}
+
+export enum Category {
+  MensClothing = "men's clothing",
+  WomensClothing = "women's clothing",
+  Jewelery = 'jewelery',
+  Electronics = 'electronics',
+}
+
+export interface Rating {
+  rate: number;
+  count: number;
+}
